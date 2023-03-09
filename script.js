42abae58b6093d2607773f5fd5d5afd74f22377f
Vikash <chiragdksh@gmail.com>
2023-03-09 06:24:19 -0800

    Updated the website

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Cloud learner", "Developer", "DevOps learner", "Designer"],
+        strings: ["DevOps Engineer", "Developer", "Cloud guy", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Cloud learner", "Developer", "DevOps learner", "Designer"],
+        strings: ["DevOps Engineer", "Developer", "Cloud guy", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
